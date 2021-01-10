ed735c20fdb5ee1f71cd419a553ca26773637aa2
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2021-01-10 13:18:08 +0530

    Firebase config

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyBJJ5pQAciLz1yArtPkJ3Uj1df3-UpCB2g',
+  authDomain: 'amzn-clone-ac9dd.firebaseapp.com',
+  projectId: 'amzn-clone-ac9dd',
+  storageBucket: 'amzn-clone-ac9dd.appspot.com',
+  messagingSenderId: '400243562767',
+  appId: '1:400243562767:web:960f66e6db3fab4282c67a',
+};
